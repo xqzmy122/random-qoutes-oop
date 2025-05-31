d984df96e7edf821e8194276166bb7abeabed7d9
Miron <mironkedich@gmail.com>
2025-05-31 19:25:00 +0300

    one more endpoint and change theme functionality

--- client/index.js
+++ client/index.js
@@ -1,3 +1,23 @@
 import RandomQuotesApp from "./src/classes/RandomQuotesApp.js";
 
 new RandomQuotesApp()
+
+const toggleThemeBtn = document.querySelector('.theme-toggle')
+const body = document.body
+
+if(localStorage.getItem('theme') === 'dark') {
+  body.classList.add('dark-theme')
+  toggleThemeBtn.textContent = '☀️'
+}
+
+toggleThemeBtn.addEventListener('click', () => {
+  body.classList.toggle('dark-theme')
+
+  if (body.classList.contains("dark-theme")) {
+    localStorage.setItem("theme", "dark");
+    toggleThemeBtn.textContent = "☀️";
+  } else {
+    localStorage.setItem("theme", "light");
+    toggleThemeBtn.textContent = "🌙";
+  }
+})
--- server/index.js
+++ server/index.js
@@ -5,15 +5,23 @@ const cors = require('cors')
 const app = express()
 const port = 3000
 
+const corsOptions = {
+  origin: 'http://127.0.0.1:8080',
+}
+
 function getRandomQuote() {
   return  quotes[Math.floor(Math.random() * quotes.length)]
 }
 
-app.use(cors())
+app.use(cors(corsOptions))
 
 app.get('/quotes/random', (req, res) => {
   res.json(getRandomQuote())
   console.log(req.headers['user-agent']);
+})
+
+app.get('/quotes', (req, res) => {
+  res.json(quotes)
 })
 
 app.listen(port, () => {
